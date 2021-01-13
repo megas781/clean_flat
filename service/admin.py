33bd4b921dcd77666145515913a3296e3b35e7c2
megas781 <megas781@gmail.com>
2021-01-13 23:01:16 +0300

    Reviews functionallity implemented

--- service/admin.py
+++ service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Service, Order
+from .models import Service, Order, Review
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Order)
+admin.site.register(Review)
